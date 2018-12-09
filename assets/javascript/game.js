0893427647e405dca4bb57a13ea945f1d3e13423
Shawn Holcomb <sholcmb@gmail.com>
2018-12-09 00:45:59 -0600

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,36 @@
 //Potential words for the Beach Life Hangman game
-var wordChoice = ["sunshine", "surfboard", "bikini"]
+var wordChoice = ["bonfire", "sunset", "boardwalk"];
 var wins = 0;
 var guessesRemaining = 6;
+var indexOfCurrentWord;
+var currentWord = [];
+var guessedLetters = [];
+
 
 //user presses a key, random word from wordChoice array is selected and displayed on screen as blanks, number of choices is reset to 7, image in left column is reset
-var userPress = document.onKeyUp(event.key)
-var wordToGuess = wordChoice[Math.floor(Math.randon()*wordChoice.length)];
+function startGame () {
+    
+    document.onKeyUp = function (event) {
+        var userPress = event.key;   
+        guessesRemaining = 6;
+        console.log(userPress);
+    }
 
 
-//user picks a letter, letter is displayed on screen, 
+}
+
+
+
+// var wordToGuess = wordChoice[Math.floor(Math.randon()*wordChoice.length)];
+
+//user picks as letter 
 
 
 
 //if letter is in word, letter is revealed, if letter is not in word it is added to the letters already guessed and # of guess is reduced by one
-var guesseRemainingElement = document.getElementById("wins");
-guesseRemainingElement = guessesRemaining;
+
+// var guesseRemainingElement = document.getElementById("wins");
+// guesseRemainingElement = guessesRemaining;
 
 
 
