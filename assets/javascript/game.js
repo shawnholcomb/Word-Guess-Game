cdfa79b55a9ef8152da726a53b662e2b606ea0a7
Shawn Holcomb <sholcmb@gmail.com>
2018-12-10 21:10:17 -0600

    complete js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,98 +1,105 @@
-//Potential words for the Beach Life Hangman game
-var words = ["bonfire"]; // "sunset", "boardwalk", "surf", "fish"
+//Global variables
+var words = ["surfboard", "wipeout", "barney", "quiver", "break"]; 
+var currentWord
 var underScore = [];
 var wins = 0;
-var guessesRemaining = 6;
+var losses = 0;
+var guessesRemaining = 9;
 var rightWord = [];
 var wrongWord = [];
-var newGame = true;
+var userGuess;
 var bonSound = new Audio('assets/audio/boardwalk.mp3');
 
-//random word is selected
+//Start game
 
-if (newGame) {
+function startGame() {
 
-var currentWord = words[Math.floor(Math.random() * words.length)];
+    //Reset for when new game starts
 
-console.log(currentWord);
+    wrongWord = [];
+    rightWord = [];
+    underScore = [];
+    guessesRemaining = 9;
+    
+    //random word is selected
 
-//current word is displayed on screen as blanks
+    currentWord = words[Math.floor(Math.random() * words.length)];
 
-var currentWordUnderscore = () => {
+    //current word is displayed on screen as blanks
+
     for (var i = 0; i < currentWord.length; i++) {
-        underScore.push('-');
+            underScore.push('-');
     }
-    return underScore;
-}
-console.log(currentWordUnderscore());    
+}  
 
-//user picks a letter 
+//User picks a letter 
 
-document.addEventListener('keypress', (event) => {
-    
-    newGame = false;
+document.onkeyup = function(event) {
+    userGuess = event.key;
 
-//determine if letter is in word
+    // //determine if letter is in word
 
-if(currentWord.indexOf(event.key) > -1) {
+    if (currentWord.indexOf(userGuess) > -1) {
 
-//if in currentWord
-    
-    rightWord.push(event.key); 
-    underScore[currentWord.indexOf(event.key)] = event.key;
-
-    if(underScore.join('') == currentWord) {
-
-//need to push to DOM here for winning game
-
-        wins = wins + 1;
+        //if in currentWord
         
-        newGame = true;
-
-        document.getElementById("you-win").innerHTML = "<h3 style='font-size: 52px;'>YOU WIN!</h3>";
-
-        if(currentWord == "sunset") {
-            document.getElementById('fill-image').src = "assets/images/sunset.jpg";
-            document.getElementById('fill-text').innerHTML = "Santa Monica, CA is known as one of the best spots in the world to watch the sunset. Over 6 million people visit the Santa Monica Pier each year where the sunset can be viewed from 130 feet in the air atop the Pacific Ferris Wheel.";
-        } 
-        else if (currentWord == "bonfire") {
-            document.getElementById('fill-image').src = "assets/images/bonfire.jpg";
-            document.getElementById('fill-text').innerHTML = "There are only 38 beaches in California that still allow bonfires.  A favorite past-time for many when the sun sets and the temperatures drop, a beach bonfire is second to none.";
-            bonSound.play();
-        }
-        else if (currentWord == "boardwalk") {
-            document.getElementById('fill-image').src = "assets/images/boardwalk.jpeg";
-            document.getElementById('fill-text').innerHTML = "The Santa Cruz Boardwalk overlooks Montery Bay and is home to the oldest amusement park in California. It features more than 40 rides and attractions, 31 restaurants and an 18-hol mini-golf course.";
+        for (var i = 0; i < currentWord.length; i++) {
+            if (currentWord[i] === userGuess) {
+                underScore[i] = userGuess;
+            }
         };
-    }
-    
-} else {
 
-//if not in currentWord
-    
-    wrongWord.push(event.key);
-    guessesRemaining = guessesRemaining - 1;
-
-//if guessesRemaining = 0 the user loses
-
-    if(guessesRemaining == 0) {
-
-// //need to push to DOM for losing game
+        if(underScore.join('') == currentWord) {
         
-        newGame = true;
+        //need to push to DOM here for winning game
 
-        document.getElementById('fill-image').src = "assets/images/sadbeach.jpg";
-        document.getElementById('fill-text').innerHTML = "<h3 style='font-size:46px; margin-top: 10px;'>Sorry You Lose!<br>Try Again</h3>";
+            wins = wins + 1;
+
+            document.getElementById("you-win").innerHTML = "<h3 style='font-size: 52px;'>COWABUNGA DUDE!</h3>";
+
+            if(currentWord == "sunset") {
+                document.getElementById('fill-image').src = "assets/images/sunset.jpg";
+                document.getElementById('fill-text').innerHTML = "Santa Monica, CA is known as one of the best spots in the world to watch the sunset. Over 6 million people visit the Santa Monica Pier each year where the sunset can be viewed from 130 feet in the air atop the Pacific Ferris Wheel.";
+            } 
+            else if (currentWord == "bonfire") {
+                document.getElementById('fill-image').src = "assets/images/bonfire.jpg";
+                document.getElementById('fill-text').innerHTML = "There are only 38 beaches in California that still allow bonfires.  A favorite past-time for many when the sun sets and the temperatures drop, a beach bonfire is second to none.";
+                bonSound.play();
+            }
+            else if (currentWord == "boardwalk") {
+                document.getElementById('fill-image').src = "assets/images/boardwalk.jpeg";
+                document.getElementById('fill-text').innerHTML = "The Santa Cruz Boardwalk overlooks Montery Bay and is home to the oldest amusement park in California. It features more than 40 rides and attractions, 31 restaurants and an 18-hol mini-golf course.";
+            };
+
+            startGame();
+        }   
+    
+    } else {
+
+        //if not in currentWord
+        
+        wrongWord.push(userGuess);
+        guessesRemaining = guessesRemaining - 1;
+
+        //if guessesRemaining = 0 the user loses
+
+        if(guessesRemaining == 0) {
+
+            losses = losses + 1;
+
+            document.getElementById('fill-image').src = "assets/images/sadbeach.jpg";
+            document.getElementById('fill-text').innerHTML = "<h3 style='font-size:46px; margin-top: 10px;'>Wipeout!!<br>Try Again</h3>";
+
+            startGame();
+        }
     }
-}
-}
 
 document.getElementById("current-word").innerHTML = underScore.join(' ');
 document.getElementById("guesses-remaining").innerHTML = guessesRemaining;
 document.getElementById("letters-guessed").innerHTML = wrongWord.join(', ');
 document.getElementById("wins").innerHTML = 'Wins: ' + wins;
+document.getElementById("losses").innerHTML = 'Losses: ' + losses;
 
+};
 
-console.log(underScore, wrongWord, guessesRemaining)
-
-});
+startGame();
