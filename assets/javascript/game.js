1aee4ad4dfbafeac7a3e34160ff2d3d295098664
Shawn Holcomb <sholcmb@gmail.com>
2018-12-10 22:57:22 -0600

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,7 +8,8 @@ var guessesRemaining = 9;
 var rightWord = [];
 var wrongWord = [];
 var userGuess;
-var bonSound = new Audio('assets/audio/boardwalk.mp3');
+var cowSound = new Audio('assets/audio/cowabunga.mp3');
+var wipeSound = new Audio('assets/audio/wipeout.mp3');
 
 //Start game
 
@@ -20,6 +21,7 @@ function startGame() {
     rightWord = [];
     underScore = [];
     guessesRemaining = 9;
+    document.getElementById("you-win").innerHTML = "";
     
     //random word is selected
 
@@ -56,19 +58,27 @@ document.onkeyup = function(event) {
             wins = wins + 1;
 
             document.getElementById("you-win").innerHTML = "<h3 style='font-size: 52px;'>COWABUNGA DUDE!</h3>";
+            cowSound.play();
 
-            if(currentWord == "sunset") {
-                document.getElementById('fill-image').src = "assets/images/sunset.jpg";
-                document.getElementById('fill-text').innerHTML = "Santa Monica, CA is known as one of the best spots in the world to watch the sunset. Over 6 million people visit the Santa Monica Pier each year where the sunset can be viewed from 130 feet in the air atop the Pacific Ferris Wheel.";
+            if(currentWord == "surfboard") {
+                document.getElementById('fill-image').src = "assets/images/surfboard.jpg";
+                document.getElementById('fill-text').innerHTML = "The surfboard was invented in ancient Hawaii where they were carved from the wood of local trees.  There have been some major advancements over the years though and currently more than 400,000 boards are manufactured each year.";
             } 
-            else if (currentWord == "bonfire") {
-                document.getElementById('fill-image').src = "assets/images/bonfire.jpg";
-                document.getElementById('fill-text').innerHTML = "There are only 38 beaches in California that still allow bonfires.  A favorite past-time for many when the sun sets and the temperatures drop, a beach bonfire is second to none.";
-                bonSound.play();
+            else if (currentWord == "wipeout") {
+                document.getElementById('fill-image').src = "assets/images/wipeout.jpg";
+                document.getElementById('fill-text').innerHTML = "A wipeout is when a surfer falls or is knocked off the board.  While underwater, the power of the wave can shake the surfer around which is called being Rag-dolled.  In fact 113 people died in 2017 while surfing.";
             }
-            else if (currentWord == "boardwalk") {
-                document.getElementById('fill-image').src = "assets/images/boardwalk.jpeg";
-                document.getElementById('fill-text').innerHTML = "The Santa Cruz Boardwalk overlooks Montery Bay and is home to the oldest amusement park in California. It features more than 40 rides and attractions, 31 restaurants and an 18-hol mini-golf course.";
+            else if (currentWord == "barney") {
+                document.getElementById('fill-image').src = "assets/images/barney.jpg";
+                document.getElementById('fill-text').innerHTML = "A Barney is a new or untalented surfer, also refered to as a Stu or a kook.  This term can also refer to someone who is ignorant of proper surf etiquette.";
+            }
+            else if (currentWord == "quiver") {
+                document.getElementById('fill-image').src = "assets/images/quiver.jpg";
+                document.getElementById('fill-text').innerHTML = "A quiver is a surfer's collection of boards.  Typically consisting of three to six boards a quiver is the surfing equivalent to a golfer's bag of clubs.";
+            }
+            else if (currentWord == "break") {
+                document.getElementById('fill-image').src = "assets/images/break.jpeg";
+                document.getElementById('fill-text').innerHTML = "A break occurs when the water swells and 'breaks', turning into waves and white water.";
             };
 
             startGame();
@@ -87,8 +97,9 @@ document.onkeyup = function(event) {
 
             losses = losses + 1;
 
-            document.getElementById('fill-image').src = "assets/images/sadbeach.jpg";
+            document.getElementById('fill-image').src = "assets/images/shark.jpg";
             document.getElementById('fill-text').innerHTML = "<h3 style='font-size:46px; margin-top: 10px;'>Wipeout!!<br>Try Again</h3>";
+            wipeSound.play();
 
             startGame();
         }
